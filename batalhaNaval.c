181c3041cb6c6c062dce775a2a02d694013a9ce2
rian606 <rianbueno222@gmail.com>
2025-08-27 23:19:42 +0000

    nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
-
 int main(){
 
-    int valor1, valor2; //valor modificavel (obs coloquei fora do for)
+    char linhas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; 
+    // vetor linha
 
-    char linhas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // vetor linha
-
-    int colunas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //vetor coluna
+    int colunas[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+    //vetor coluna
 
     int mar[10][10] = {
-        {0 ,0 ,3 ,3 ,3 ,0 ,0 ,0 ,0 ,0},
-        {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
-        {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
+        {0 ,0 ,3 ,3 ,3 ,0 ,0 ,0 ,3 ,0},
+        {0 ,0 ,0 ,0 ,0 ,0 ,0 ,3 ,0 ,0}, 
+        {0 ,0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0},
         {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
         {0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0},   //matriz 10 X 10
-        {0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0},
-        {0 ,0 ,0 ,0 ,0 ,3 ,0 ,0 ,0 ,0},
-        {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
+        {0 ,0 ,0 ,3 ,0 ,3 ,0 ,0 ,0 ,0},
+        {0 ,0 ,3 ,0 ,0 ,3 ,0 ,0 ,0 ,0},
+        {0 ,3 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
         {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0},
         {0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}
     };
 
-    printf("   %c %c %c %c %c %c %c %c %c %c\n", linhas[0], linhas[1], linhas[2], linhas[3],
-         linhas[4], linhas[5], linhas[6], linhas[7], linhas[8], linhas[9]); //print das linhas, como nao é necessario repeticao deixei fora do for
+    printf("   %c %c %c %c %c %c %c %c %c %c\n ", linhas[0], linhas[1], linhas[2], linhas[3],
+         linhas[4], linhas[5], linhas[6], linhas[7], linhas[8], linhas[9]);
+        //print das linhas, como nao é necessario 
+        //repeticao deixei fora do for
 
-    for (valor1 = 0 ; valor1 < 9; valor1++) // abrindo for com suas especificaçoes
-    {
-        printf(" %d %d %d %d %d %d %d %d %d %d %d\n", colunas[valor1], mar[valor1][0], mar[valor1][1], mar[valor1][2], mar[valor1][3],
-        mar[valor1][4], mar[valor1][5], mar[valor1][6], mar[valor1][7], mar[valor1][8], mar[valor1][9]);
-    } //montando o codigo
+        for (int i = 0; i < 10; i++)
+        {
+            printf("%d", colunas[i]);
 
-     printf("%d %d %d %d %d %d %d %d %d %d %d\n", colunas[valor1], mar[valor1][0], mar[valor1][1], mar[valor1][2], mar[valor1][3],
-        mar[valor1][4], mar[valor1][5], mar[valor1][6], mar[valor1][7], mar[valor1][8], mar[valor1][9]);
-            //deixei a ultima linha fora apenas para encaixar corretamente o tabuleiro
+            for (int j = 0; j < 10; j++)// modifiquei o for, mais compacto e facil entendimento
+            {
+                printf(" %d", mar[i][j]);// exibicao das matrizes
+            }
+            printf("\n ");// apenas pra dar uma linha a cada matriz 
+        }
+        
     return 0;
 
 
